fc717822ed0cca5001070272905939b865a2ace1
Marc MacLeod <help@dayjot.com>
2014-11-25 11:07:27 -0500

    show logging in feedback to user

--- ember/app/controllers/login.js
+++ ember/app/controllers/login.js
@@ -1,25 +1,30 @@
-// import Ember from "ember";
-// import AuthenticationControllerMixin from 'simple-auth/mixins/authentication-controller-mixin';
-
-// export default Ember.Controller.extend(AuthenticationControllerMixin, {
-//   authenticator: "authenticator:parse",
-//   working: false,
-
-//   actions: {
-//     authenticate: function() {
-//       this.set("working", true);
-//       var data = this.getProperties('identification', 'password');
-//       data.identification = data.identification.toLowerCase();
-//       data.controller = this;
-//       this.set('password', null);
-//       return this._super(data);
-//     }
-//   }
-// });
-
 import Ember from "ember";
-import LoginControllerMixin from 'simple-auth/mixins/login-controller-mixin';
 
-export default Ember.Controller.extend(LoginControllerMixin, {
-  authenticator: 'simple-auth-authenticator:devise'
+export default Ember.Controller.extend({
+  authenticator: 'simple-auth-authenticator:devise',
+
+  identification: null,
+  password: null,
+  error: null,
+  working: false,
+
+  actions: {
+    authenticate: function() {
+      var _this = this,
+          data = this.getProperties('identification', 'password');
+
+      this.setProperties({
+        working: true,
+        password: null,
+        error: null
+      });
+      
+      this.get('session').authenticate('simple-auth-authenticator:devise', data).then(function() {
+        // authentication was successful
+      }, function(data) {
+        _this.set('working', false);
+        _this.set('error', data.error);
+      });
+    }
+  }
 });
